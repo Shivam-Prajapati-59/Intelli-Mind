8e4152aa39deea35e890baacf45c532d3fc096db
Prajapati Shivam <113005045+Prajapatishivam65@users.noreply.github.com>
2025-03-17 22:27:51 +0530

    Updated Gemini Modal

--- app/api/coding-questions/route.ts
+++ app/api/coding-questions/route.ts
@@ -34,7 +34,7 @@ const initializeGeminiApi = () => {
   }
 
   const genAI = new GoogleGenerativeAI(apiKey);
-  return genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  return genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 };
 
 const generatePrompt = (topic: string): string => {
