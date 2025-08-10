22a5d2d8ae00ad2a048f89cc61d22f03465fd51f
Prajapatishivam65 <sp488830@gmail.com>
2025-08-10 18:31:57 +0530

    Update model in GoogleGenerativeAI and configure database URL from environment variable

--- app/api/coding-questions-feedback/route.ts
+++ app/api/coding-questions-feedback/route.ts
@@ -12,7 +12,7 @@ if (!apiKey) {
 
 const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
+  model: "gemini-1.5-flash",
 });
 
 const generationConfig = {
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./utils/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_TQ6vBrhgc9Cj@ep-shy-darkness-a1wok5n0-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL!,
   },
 });
